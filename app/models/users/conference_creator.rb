e884c66c4ed0c486e3bfca1d3bf548f32f4150ef
Vorobyov-Roman <vorobrawr@gmail.com>
2016-05-11 09:48:18 +0300

    conference creator now can assign reviewers

--- app/models/users/conference_creator.rb
+++ app/models/users/conference_creator.rb
@@ -1,4 +1,8 @@
 class ConferenceCreator < ActiveRecord::Base
   belongs_to :user
   belongs_to :conference
+
+  def add_reviewer(user, topic)
+    topic.reviewers << TopicReviewer.create(user: user)
+  end
 end
